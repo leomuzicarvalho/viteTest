26921d506309984dc0616ebd86c522731fad6b2f
Leonardo Muzi <leomuzicarvalho@gmail.com>
2022-05-15 12:18:26 +0100

    inital commit

---
+++ src/components/HelloWorld.test.ts
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/vue';
+import HelloWorld from './HelloWorld.vue';
+
+test('it should display text passed as prop', () => {
+  const { getByText } = render(
+    HelloWorld,
+    {
+      props: {
+        msg: 'Hi vitest'
+      }
+    }
+  );
+
+  getByText('Hi vitest');
+});
